7f917a0131260bdac808f2f08cd343fb985eea3e
Fionanguessan <fiona.nguessan@hotmail.fr>
2023-04-11 12:15:58 +0200

    ajout methode findByQuestion ds ReponseRepo Fiona

--- projetQuizz/src/main/java/projetQuizz/repositories/ReponseRepository.java
+++ projetQuizz/src/main/java/projetQuizz/repositories/ReponseRepository.java
@@ -2,13 +2,20 @@ package projetQuizz.repositories;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import projetQuizz.entities.Question;
 import projetQuizz.entities.Reponse;
 
 public interface ReponseRepository extends JpaRepository<Reponse, Long>{
     public List<Reponse> findByEnonceReponse(String enonceReponse);
 
 	public List<Reponse> findByEnonceReponseContaining(String enonceReponse);
-
+    
+    @Transactional
+    @Modifying
+	public List<Reponse> findByQuestion(Question question);
 }
