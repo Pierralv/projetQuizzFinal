b6295ba8aafb65ac1619364bad4a1692288b3a3b
Fionanguessan <fiona.nguessan@hotmail.fr>
2023-04-11 12:31:06 +0200

    modif ReponseServiceTest Fiona

--- projetQuizz/src/test/java/projetQuizz/ReponseServiceTest.java
+++ projetQuizz/src/test/java/projetQuizz/ReponseServiceTest.java
@@ -13,7 +13,7 @@ import projetQuizz.services.ReponseService;
 
 @SpringJUnitConfig(JpaConfig.class)
 @Transactional
-class ReponseServiceTest {	
+public class ReponseServiceTest {	
 	@Autowired
 	ReponseService reponseService;
 	
@@ -21,8 +21,14 @@ class ReponseServiceTest {
 	@Test
 	// @Commit
 	void test() {
-		Reponse reponse = new Reponse("énoncéReponse1", true);
-		reponseService.createOrUpdate(reponse);
+		Reponse reponse1 = new Reponse("proposition1SurUnFilm", true);
+		Reponse reponse2 = new Reponse("proposition2SurUnFilm", false);
+		Reponse reponse3  = new Reponse("proposition3SurUnFilm", false);
+		Reponse reponse4  = new Reponse("proposition4SurUnFilm", false);
+		reponseService.createOrUpdate(reponse1);
+		reponseService.createOrUpdate(reponse2);
+		reponseService.createOrUpdate(reponse3);
+		reponseService.createOrUpdate(reponse4);
 
 		System.out.println("------------getAll------------");
 		System.out.println(reponseService.getAll());
@@ -31,10 +37,10 @@ class ReponseServiceTest {
 		System.out.println(reponseService.getById(1L));
 
 		System.out.println("------------getByEnonce------------");
-		System.out.println(reponseService.getByEnonce("énoncéReponse1"));
+		System.out.println(reponseService.getByEnonce("proposition2SurUnFilm"));
 
 		System.out.println("------------getByEnonceContaining------------");
-		System.out.println(reponseService.getByEnonceContaining("Reponse1"));
+		System.out.println(reponseService.getByEnonceContaining("4"));
 	}
 
 }
