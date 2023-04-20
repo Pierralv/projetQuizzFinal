40eb76c9ce8be9131def848c29ff8fee5250fc1e
Fionanguessan <fiona.nguessan@hotmail.fr>
2023-04-20 18:06:42 +0200

    modif SecurityConfig

--- projetQuizzSpringBoot/src/main/java/ajc/formation/soprasteria/projetQuizzSB/config/SecurityConfig.java
+++ projetQuizzSpringBoot/src/main/java/ajc/formation/soprasteria/projetQuizzSB/config/SecurityConfig.java
@@ -16,19 +16,31 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //@formatter:off
 		return http.antMatcher("/api/**")
-        .csrf().disable()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authorizeRequests()
-            .antMatchers(HttpMethod.POST, "/api/compte/inscription").anonymous()
-            .antMatchers(HttpMethod.PUT, "/api/compte/**/byadmin").hasAnyRole("ADMIN")
-            .antMatchers(HttpMethod.GET, "/api/compte/all").hasAnyRole("ADMIN")
-            .antMatchers(HttpMethod.GET, "/api/compte/profil**").hasAnyRole("ADMIN","UTILISATEUR")
-            .antMatchers("").permitAll()
-        .and()
-        .httpBasic()
-        .and()
-        .build();
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                //compte
+                .antMatchers(HttpMethod.POST, "/api/compte/inscription").anonymous()
+                .antMatchers(HttpMethod.PUT, "/api/compte/**/byadmin").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/compte/all").hasAnyRole("ADMIN") //acces à la liste des comptes
+                .antMatchers(HttpMethod.GET, "/api/compte/profil**").hasAnyRole("ADMIN","UTILISATEUR") //acces aux profils individuels
+                //theme
+                .antMatchers(HttpMethod.POST, "/api/theme").hasAnyRole("ADMIN") //acces à la creation de question
+                .antMatchers(HttpMethod.PUT, "/api/theme/**").hasAnyRole("ADMIN") //acces à la creation de question
+                .antMatchers(HttpMethod.DELETE, "/api/theme/**").hasAnyRole("ADMIN") //acces à la creation de question
+                //question
+                .antMatchers(HttpMethod.POST, "/api/question").hasAnyRole("ADMIN") //acces à la creation de question
+                .antMatchers(HttpMethod.PUT, "/api/question/**").hasAnyRole("ADMIN") //acces à la modif de question
+                .antMatchers(HttpMethod.DELETE, "/api/question/**").hasAnyRole("ADMIN") //acces à la suppression de question
+                //reponse
+                .antMatchers(HttpMethod.POST, "/api/reponse").hasAnyRole("ADMIN") //acces à la creation de question
+                .antMatchers(HttpMethod.PUT, "/api/reponse/**").hasAnyRole("ADMIN") //acces à la modif de question
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .build();
 //@formatter:on	
     }
 
