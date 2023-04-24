2309d011d8cf2e8a5acab616dcaccd0e1a222546
augustinBenard <augustin.benard@hotmail.com>
2023-04-24 10:52:21 +0200

    mise à jour du rooting

--- projetQuizzAngular/src/app/app-routing.module.ts
+++ projetQuizzAngular/src/app/app-routing.module.ts
@@ -3,10 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ConnexionComponent } from './components/connexion/connexion.component';
 import { AnonymousGuardService } from './services/guard/anonymous-guard.service';
+import { ProfilComponent } from './components/profil/profil.component';
+import { InscriptionComponent } from './components/inscription/inscription.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'connexion', component: ConnexionComponent,canActivate: [AnonymousGuardService]}
+  {path: 'connexion', component: ConnexionComponent,canActivate: [AnonymousGuardService]},
+  {path: 'profil', component: ProfilComponent },
+  {path: 'inscription', component: InscriptionComponent},
+  {path: '', redirectTo:'/home', pathMatch:'full'},
+  {path: 'quizz', component: QuizzComponent}
 ];
 
 
