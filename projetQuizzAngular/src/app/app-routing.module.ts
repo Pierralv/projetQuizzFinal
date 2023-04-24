09aa35162cfb9ec07389c6f01b94d8b26fee9d29
augustinBenard <augustin.benard@hotmail.com>
2023-04-24 12:37:15 +0200

    Nav-Bar V.0

--- projetQuizzAngular/src/app/app-routing.module.ts
+++ projetQuizzAngular/src/app/app-routing.module.ts
@@ -6,14 +6,18 @@ import { AnonymousGuardService } from './services/guard/anonymous-guard.service'
 import { ProfilComponent } from './components/profil/profil.component';
 import { InscriptionComponent } from './components/inscription/inscription.component';
 import { QuizzComponent } from './components/quizz/quizz.component';
+import { StatistiquesComponent } from './components/statistiques/statistiques.component';
+import { ThemeComponent } from './components/theme/theme.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'connexion', component: ConnexionComponent,canActivate: [AnonymousGuardService]},
-  {path: 'profil', component: ProfilComponent },
-  {path: 'inscription', component: InscriptionComponent},
+  {path: 'profil', component: ProfilComponent},
+  {path: 'inscription', component: InscriptionComponent, canActivate: [AnonymousGuardService]},
   {path: '', redirectTo:'/home', pathMatch:'full'},
-  {path: 'quizz', component: QuizzComponent}
+  {path: 'quizz', component: QuizzComponent},
+  {path: 'statistiques', component : StatistiquesComponent},
+  {path: 'theme', component : ThemeComponent }
 ];
 
 
--- projetQuizzAngular/src/app/components/nav-bar/nav-bar.component.ts
+++ projetQuizzAngular/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Compte } from 'src/app/model/compte';
+import { Role } from 'src/app/model/role';
 
 @Component({
   selector: 'app-nav-bar',
@@ -13,5 +15,28 @@ export class NavBarComponent {
     sessionStorage.clear();
     this.router.navigateByUrl('/home');
   }
+
+  get connecte(): boolean {
+    return sessionStorage.getItem('token') ? true : false;
+  }
+
+  get admin(): boolean {
+    if (sessionStorage.getItem('compte')) {
+      let compte: Compte = JSON.parse(
+        sessionStorage.getItem('compte')!
+      ) as Compte
+      return compte.role == Role.ROLE_ADMIN;
+    }
+    return false;
+  }
+get utilisateur(): boolean {
+  if (sessionStorage.getItem('compte')) {
+    let compte: Compte = JSON.parse(
+      sessionStorage.getItem('compte')!
+    ) as Compte;
+    return compte.role == Role.ROLE_UTILISATEUR;
+  }
+  return false
+}
 
 }
