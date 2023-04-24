2efd8f3319e710a331236439b84e48520414bd48
Fionanguessan <fiona.nguessan@hotmail.fr>
2023-04-24 10:54:55 +0200

    modif env.ts

--- projetQuizzAngular/src/app/env.ts
+++ projetQuizzAngular/src/app/env.ts
@@ -1,4 +1,7 @@
-export const srvRest: string = 'http://localhost:8080/quizz/api';
+export const srvRest: string = "http://localhost:8080/quizz/api";
 
 export const authRest: string = `${srvRest}/auth`;
 export const compteRest: string = `${srvRest}/compte`;
+export const questionRest: string = `${srvRest}/question`;
+export const reponseRest: string = `${srvRest}/reponse`;
+export const themeRest: string = `${srvRest}/theme`;
