e3b8022e06fcebc19c3eeaa8461efae7e38ef844
Fionanguessan <fiona.nguessan@hotmail.fr>
2023-04-26 18:38:35 +0200

    modif component quizz/question

--- projetQuizzAngular/src/app/components/quizz/question/question.component.ts
+++ projetQuizzAngular/src/app/components/quizz/question/question.component.ts
@@ -1,6 +1,7 @@
 import { QuizzService } from "src/app/services/quizz.service";
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { Question } from "src/app/model/question";
+import { Reponse } from "src/app/model/reponse";
 
 @Component({
 	selector: "app-question",
@@ -8,9 +9,6 @@ import { Question } from "src/app/model/question";
 	styleUrls: ["./question.component.css"],
 })
 export class QuestionComponent {
-	value!: string;
-	bonneReponse!: boolean;
-
 	@Input()
 	numero: number;
 
@@ -23,16 +21,27 @@ export class QuestionComponent {
 	@Output()
 	showQuestionEven: EventEmitter<boolean> = new EventEmitter();
 
+	value!: string;
+	bonneReponse!: Reponse;
+
 	constructor(private quizzSrv: QuizzService) {}
 
 	getValue(ev: Event) {
 		this.value = (ev.target as HTMLInputElement).value;
-		if (this.value) {
-		}
+		console.log(this.value);
+		// if (this.value == "true") {
+		// 	console.log("if vrai");
+		// } else {
+		// 	console.log("else");
+		// }
 	}
 
 	voirResults() {
 		this.showQuestion = false;
 		this.showQuestionEven.emit(this.showQuestion);
+		this.question.reponses.forEach((rep) => {
+			if (rep.bonneReponse) this.bonneReponse = rep;
+		});
+		console.log(this.bonneReponse);
 	}
 }
